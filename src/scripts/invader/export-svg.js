a33845154217d0cd53cad121169d1edc64212ff6
marklabenski <mark.labenski@gmail.com>
2016-11-12 18:22:26 +0100

    fix: move rectangles in svg correctly

--- src/scripts/invader/export-svg.js
+++ src/scripts/invader/export-svg.js
@@ -4,12 +4,15 @@
 
 const exportSvg = function(invader) {
   const pixelsArray = invader.appearance.pixels;
-  let svg = pixelsArray.reduce((svgString, pixelVal) => {
+  let svg = pixelsArray.reduce((svgString, pixelVal, index) => {
     let color = 'rgb(0,0,0)';
     if(pixelVal === true) {
       color = 'rgb(255,255,255)';
     }
-    return svgString + '<rect x="0" y="0" width="1" height="1" style="fill:' + color + '; stroke-width:0" />\n';
+    const col = index % 9;
+    const row = Math.floor(index / 9);
+
+    return svgString + '<rect x="' + col + '" y="' + row + '" width="1" height="1" style="fill:' + color + '; stroke-width:0" />\n';
   }, '<svg width="9" height="9">\n');
   return svg + '</svg>';
 };
--- test/export-svg.js
+++ test/export-svg.js
@@ -10,7 +10,7 @@ describe('exportSvg', function() {
       }};
       expect(exportSvg(invaderMock)).to.equal('<svg width="9" height="9">\n' +
         '<rect x="0" y="0" width="1" height="1" style="fill:rgb(255,255,255); stroke-width:0" />\n' +
-        '<rect x="0" y="0" width="1" height="1" style="fill:rgb(0,0,0); stroke-width:0" />\n' +
+        '<rect x="1" y="0" width="1" height="1" style="fill:rgb(0,0,0); stroke-width:0" />\n' +
         '</svg>');
     });
   });
