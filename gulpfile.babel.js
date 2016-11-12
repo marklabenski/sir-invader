16cfbdaca92a311148f3ad4cf29afe5364b8c8dd
marklabenski <mark.labenski@gmail.com>
2016-11-12 15:56:16 +0100

    simplify workflow throw out webpack

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,22 +2,13 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import mocha from 'gulp-mocha';
 import gutil from 'gulp-util';
-import webpack from 'webpack';
-import webpackConfig from './webpack.config.babel';
-import ghPages from 'gulp-gh-pages';
 
-gulp.task('deploy', function() {
-  gulp.start('default');
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
-});
-
-gulp.task('default', ['webpack']);
+gulp.task('default', ['test']);
 
 gulp.task('babel', () => {
   return gulp.src('src/scripts/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest('target'));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('test', ['babel'], () => {
@@ -35,22 +26,4 @@ gulp.task('watch-test', () => {
 gulp.task('watch', () => {
   gulp.start('default');
   return gulp.watch(['src/**'], ['default']);
-});
-
-gulp.task('webpack', ['test'], function(callback) {
-  var myConfig = Object.create(webpackConfig);
-  myConfig.plugins = [
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.UglifyJsPlugin()
-  ];
-
-  // run webpack
-  webpack(myConfig, function(err, stats) {
-    if (err) throw new gutil.PluginError('webpack', err);
-    gutil.log('[webpack]', stats.toString({
-      colors: true,
-      progress: true
-    }));
-    callback();
-  });
 });
